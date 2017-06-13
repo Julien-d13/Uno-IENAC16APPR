7400cb8f41186320b8931dc56afb8cb12d8b901f
Swishmann <bittermannhugo@hotmail.fr>
2017-06-13 10:33:46 +0200

    13/06/2017 (3)

--- projet-python-uno.py
+++ projet-python-uno.py
@@ -16,7 +16,7 @@ from numpy import *
 # Fonctions
 ###################################################################
 
-def nbr_joueurs():
+def liste_joueurs():
     player = []
     print("A combien voulez-vous jouer ?")
     a = int(input())
@@ -70,7 +70,6 @@ def creation_jeu_de_cartes():
     
     
 def distribution(nbr, jeu_de_cartes):
-    nbr = len(nbr)
     cartes_joueurs = []
     for i in range(0,nbr):
         cartes_joueurs.append([0]*108) #creation d'une liste de liste pouvant contenir les cartes des joueurs
@@ -79,15 +78,26 @@ def distribution(nbr, jeu_de_cartes):
             cartes_joueurs[k][j] = jeu_de_cartes[0]
             del jeu_de_cartes[0]
     return cartes_joueurs
+     
+def qui_commence(nbr):
+    joueur_qui_commence=randint(0,nbr -1)
+    return joueur_qui_commence        
         
-        
-def fausse():
+def fausse(jeu_de_cartes):
     fausse = []
     fausse.append(jeu_de_cartes[0])
-            
+    return fausse
     
-    
+def afficher_cartes(cartes_joueurs):
+    i = 0
+    cartes_utilisateur = []
+    while cartes_joueurs[0][i]!= 0:
+        cartes_utilisateur.append(cartes_joueurs[0][i])
+        i = i+1
+    return cartes_utilisateur
 
+#def jeu(nbr,jeu_de_cartes, joueur_qui_commence, fausse, liste_joueurs, cartes_joueurs):
+    
 
         
     
@@ -95,11 +105,17 @@ def fausse():
 # Script principal de test
 ###################################################################
 
-nbr = nbr_joueurs()
+liste_joueurs = liste_joueurs()
+nbr = len(liste_joueurs)
 jeu_de_cartes = creation_jeu_de_cartes()
 cartes_joueurs = distribution(nbr, jeu_de_cartes)
-
-
+joueur_qui_commence = qui_commence(nbr)
+fausse = fausse(jeu_de_cartes)
+joueur_commencant = liste_joueurs[joueur_qui_commence]
+print('Le joueur débutant la partie est le ',joueur_commencant)
+cartes_utilisateur = afficher_cartes(cartes_joueurs)
+print('Vos cartes sont :',cartes_utilisateur)
+#jeu(nbr,jeu_de_cartes, joueur_qui_commence, fausse, liste_joueurs)
 
 
 ###################################################################
