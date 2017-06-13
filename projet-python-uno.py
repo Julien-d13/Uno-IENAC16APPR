9744667a5b19e6b3d49b8c16458935448c644531
Julien-d13 <julien.rapin13@gmail.com>
2017-06-13 08:55:31 +0200

    13/06/17

--- projet-python-uno.py
+++ projet-python-uno.py
@@ -20,49 +20,54 @@ def nbr_joueurs():
     player = []
     print("A combien voulez-vous jouer ?")
     a = int(input())
-    for i in range(1,a+1):
-        i=str(i)
-        player.append('Joueur ' + i)
-    print(player)
+    if a < 2 or a > 10:
+        print("Le uno se joue de 2 à 10 joueurs.")
+        player = nbr_joueurs()
+    else:
+        for i in range(1,a+1):
+            i=str(i)
+            player.append('Joueur ' + i)
+    return player
+    
 
 
 def creation_jeu_de_cartes():
-    B=['B 0']
+    B = ['B 0']
     for i in range(1,10):
-        i=str(i)
+        i = str(i)
         B.append('B '+i)
         B.append('B '+i)
-    B=B+['B Passe']*2 + ['B +2']*2 + ['B Inverse']*2
+    B = B+['B Passe']*2 + ['B +2']*2 + ['B Inverse']*2
     
     R=['R 0']
     for i in range(1,10):
-        i=str(i)
+        i = str(i)
         R.append('R '+i)
         R.append('R '+i)
-    R=R+['R Passe']*2 + ['R +2']*2 + ['R Inverse']*2
+    R = R+['R Passe']*2 + ['R +2']*2 + ['R Inverse']*2
     
-    V=['V 0']
+    V = ['V 0']
     for i in range(1,10):
         i=str(i)
         V.append('V '+i)
         V.append('V '+i)
-    V=V+['V Passe']*2 + ['V +2']*2 + ['V Inverse']*2
+    V = V+['V Passe']*2 + ['V +2']*2 + ['V Inverse']*2
     
-    J=['J 0']
+    J = ['J 0']
     for i in range(1,10):
         i=str(i)
         J.append('J '+i)
         J.append('J '+i)
-    J=J+['J Passe']*2 + ['J +2']*2 + ['J Inverse']*2
+    J = J+['J Passe']*2 + ['J +2']*2 + ['J Inverse']*2
     
-    Joker=['Joker']*4
-    Super_Joker=['Joker +4']*4
+    Joker = ['Joker']*4
+    Super_Joker = ['Joker +4']*4
     
-    Jeu_de_cartes=B+R+V+J+Super_Joker+Joker
+    jeu_de_cartes = B  + R + V + J + Super_Joker + Joker
     
-    shuffle(Jeu_de_cartes)
+    shuffle(jeu_de_cartes)
     
-    return Jeu_de_cartes
+    return jeu_de_cartes
     
     
 def distribution(nbr_joueurs):
@@ -81,7 +86,7 @@ def distribution(nbr_joueurs):
 ###################################################################
 
 jeu_de_carte = creation_jeu_de_cartes()
-nbr_joueurs()
+nbr = nbr_joueurs()
 
 
 
